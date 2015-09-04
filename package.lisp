66b066844feefb5f9f99bb71b496cd77b41610ff
Shinmera <shinmera@tymoon.eu>
2015-09-04 21:36:40 +0200

    Exports

--- package.lisp
+++ package.lisp
@@ -8,5 +8,32 @@
 (defpackage #:ubiquitous
   (:nicknames #:org.shirakumo.ubiquitous)
   (:use #:cl)
+  ;; accessor.lisp
   (:export
-   ))
+   #:field
+   #:remfield
+   #:augment)
+  ;; config.lisp
+  (:export
+   #:*commit*
+   #:value
+   #:remvalue
+   #:defaulted-value
+   #:call-with-transaction
+   #:with-transaction)
+  ;; pathname.lisp
+  (:export
+   #:config-directory
+   #:config-pathname
+   #:designator-pathname)
+  ;; storage.lisp
+  (:export
+   #:*storage*
+   #:*storage-type*
+   #:*storage-pathname*
+   #:read-storage
+   #:write-storage
+   #:restore
+   #:offload
+   #:define-ubiquitous-writer
+   #:define-ubiquitous-reader))
